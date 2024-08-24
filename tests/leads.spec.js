3e2a4ab3790c952f57fe132322c2347f51814831
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-08-24 16:00:51 -0300

    adicionando estrutura inicial

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -3,7 +3,13 @@ const { test, expect } = require('@playwright/test');
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem"]'}).click()
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
 
-  
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  await page.getByPlaceholder('Seu nome completo').fill('joao tadeu');
+  await page.getByPlaceholder('Seu email principal').fill('joaotadeu@outlook.com');
+
 });
