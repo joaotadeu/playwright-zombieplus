a127653cc9f7b8b17a08ff2d9e58ce50dfccb42e
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-08-24 15:22:46 -0300

    adicionando estrutura inicial

---
+++ tests/leads.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem"]'}).click()
+
+  
+});
