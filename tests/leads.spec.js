0c6702d559270f9c204ec6d810cb5f61df20b8fa
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2025-02-08 15:16:47 -0300

    adicionando

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -39,7 +39,7 @@ test('não deve cadastrar um lead na fila de espera', async ({ page }) => {
 
 });
 
-test('campos obrigatorios', async ({ page }) => {
+test('campos incorretos', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await page.getByRole('button', { name: /Aperte o play/ }).click()
 
@@ -54,5 +54,26 @@ test('campos obrigatorios', async ({ page }) => {
     .click();
 
   await expect(page.locator('.alert')).toHaveText('Email incorreto')
+
+});
+
+test('não deve preencher quando nenhum campo é preenchido', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera');
+
+  await page.getByPlaceholder('Seu nome completo').fill('joao tadeu');
+  await page.getByPlaceholder('Seu email principal').fill('joaotadeu@.com');
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!')
+    .click();
+
+  await expect(page.locator('.alert')).toHaveText([
+    'Campo obrigatório',
+    'Campo obrigatório'
+  ])
 
 });
